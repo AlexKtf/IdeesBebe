3c910ecbdc0e1c30a335a9dd1a59cb247093f5c3
AlexKtf <alexandre.ktifa@gmail.com>
2014-05-14 20:21:01 +0200

    Send emails to interested people when product is selled

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -17,11 +17,14 @@ require 'spec_helper'
 describe Status do
 
   describe 'when updates' do
-    let(:product) { FactoryGirl.create :product, owner: user }
-    let(:user) { FactoryGirl.create :user, response_time: 0 }
-    let(:user2) { FactoryGirl.create :user, response_time: 0 }
-    subject { FactoryGirl.create :status, user_id: user2.id, product: product }
-    let(:message) { FactoryGirl.create :message, sender_id: user2.id, status: subject, receiver_id: user.id, content: 'test'}
+    let(:product) { create :product, owner: user }
+    let(:user) { create :user, response_time: 0 }
+    let(:user2) { create :user, response_time: 0 }
+    let(:user3) { create :user, response_time: 0 }
+    subject { create :status, user_id: user2.id, product: product }
+    let(:status2) { create :status, user_id: user3.id, product: product }
+    let(:message) { create :message, sender_id: user2.id, status: subject, receiver_id: user.id, content: 'test'}
+    let(:message2) { create :message, sender_id: user3.id, status: status2, receiver_id: user.id, content: 'test'}
 
     context 'when the selling the product' do
       
@@ -31,12 +34,22 @@ describe Status do
         subject.product.reload.selled?.should == true
       end
 
-      it 'sends an email' do
+      it 'sends an email to the buyer' do
         message
+        message2
         expect {
           subject.update_attributes!(done: true)
           Delayed::Worker.new.work_off
         }.to change{deliveries_with_subject(I18n.t('notifier.signalized_as_buyer.subject', product: product.name)).count}.by 1
+      end
+
+      it 'sends an email to the other buyer' do
+        message
+        message2
+        expect {
+          subject.update_attributes!(done: true)
+          Delayed::Worker.new.work_off
+        }.to change{deliveries_with_subject(I18n.t('notifier.product_not_available_anymore.subject', product: product.name)).count}.by 1
       end
     end
   end
